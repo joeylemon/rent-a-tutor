466f31a1c5e9d7a9a5aefbc8368934ba8b674da2
joeylemon <joeyclemon@gmail.com>
2020-09-24 20:02:54 +0000

    add tests

--- routes/docs/router.js
+++ routes/docs/router.js
@@ -3,25 +3,22 @@ import path from 'path'
 import fs from 'fs'
 import apidoc from 'apidoc'
 
+/**
+ * Automatically generate documentation files from in-line comments in the project
+ */
 const doc = apidoc.createDoc({
     src: path.resolve(),
     dest: '/tmp/apidoc',
-    //template: "./routes/docs/template",
-    excludeFilters: ['node_modules', './routes/docs/html', './routes/docs/template']
+    excludeFilters: ['node_modules', './routes/docs/html']
 })
 
+/**
+ * Apidoc.js will overwrite all files in the destination folder, removing any custom styles we added to the page.
+ * Instead, let's call createDoc() in another folder and write only the data files in our custom html.
+ */
 fs.writeFileSync("./routes/docs/html/api_data.js", `define({ "api": ${doc.data} });`)
 fs.writeFileSync("./routes/docs/html/api_project.js", `define(${doc.project});`)
 
 const router = express.Router()
-
 router.use(express.static("./routes/docs/html"))
-// router.use(express.static("./routes/docs/html/template"))
-// router.get("/api_data.js", (req, res) => {
-//     res.sendFile("/home/dustin/rat/routes/docs/html/api_data.js")
-// })
-// router.get("/api_project.js", (req, res) => {
-//     res.sendFile("/home/dustin/rat/routes/docs/html/api_project.js")
-// })
-
 export default router
