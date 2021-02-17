02f026d4299e4abe6dafcebe9ffb4c4f945bb534
joeylemon <joeyclemon@gmail.com>
2021-02-17 05:47:33 +0000

    use pino-pretty on log output

--- src/routes/docs/html/api_project.js
+++ src/routes/docs/html/api_project.js
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-02-17T05:33:41.428Z",
+    "time": "2021-02-17T05:47:02.142Z",
     "url": "https://apidocjs.com",
     "version": "0.25.0"
   }
