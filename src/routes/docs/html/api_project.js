59ec1dc0bb6a78bd807ead4b044473be1e9c8baa
joeylemon <joeyclemon@gmail.com>
2021-03-06 01:29:08 +0000

    expand test coverage

--- src/routes/docs/html/api_project.js
+++ src/routes/docs/html/api_project.js
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-03-06T00:41:37.765Z",
+    "time": "2021-03-06T01:05:01.787Z",
     "url": "https://apidocjs.com",
     "version": "0.25.0"
   }
